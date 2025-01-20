1572128e287c8c72273b8c83e17cf279e9229cb6
BhargavKrishna484 <vaddadibhargavkrishna@gmail.com>
2025-01-20 12:16:13 +0530

    Readastringandprintit.c

---
+++ Readastringandprintit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str[100];
+    printf("enter a string:");
+    scanf("%s",str);
+    printf("the given string is %s",str);
+    return 0;
+}
